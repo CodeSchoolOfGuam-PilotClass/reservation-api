d5f6d8833951b23f3c48997030e9a749b1897e5f
AP-Lett <audreana.lett@gmail.com>
2025-02-28 20:56:25 +1000

    controllers and seed

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,40 @@
+class ReservationsController < ApplicationController
+  def index
+    reservation = Reservation.create!(
+      guest_name: params[:guest_name],
+      party_size: params[:party_size],
+      cancelled: false,
+      time: params[:time],
+      date: params[:date],
+      restaurant_id: params[restaurant_id])
+
+    render json: reservation
+  end
+
+  def show
+    reservation = Reservation.find(params[:id])
+    render json: reservation
+  end
+
+  def create
+    reservation = Reservation.create!(
+      guest_name: params[:guest_name],
+      party_size: params[:party_size],
+      cancelled: false,
+      time: params[:time],
+      date: params[:date],
+      restaurant_id: params[restaurant_id])
+
+    render json: reservation
+  end
+
+  def update
+    reservation = Reservation.find(params[:id])
+    reservation.update(
+      party_size: params[:party_size] || reservation.party_size,
+      cancelled: false || reservation.cancelled,
+      time: params[:time] || reservation.time,
+      date: params[:date] || reservation.date,
+      restaurant_id: params[restaurant_id] || reservation.restaurant_id)
+  end
+end
